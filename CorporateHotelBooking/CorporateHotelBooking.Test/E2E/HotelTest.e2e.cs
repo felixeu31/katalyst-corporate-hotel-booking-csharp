88422b76ac51de02f4fd46f1218f18e4224046f3
Félix Díez Arias <fdiez@tunstall.es>
2023-05-18 17:16:57 +0200

    chore: remove API implementation to simplify kata

--- CorporateHotelBooking/CorporateHotelBooking.Test/E2E/HotelTest.e2e.cs
+++ CorporateHotelBooking/CorporateHotelBooking.Test/E2E/HotelTest.e2e.cs
@@ -1,34 +1,29 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Json;
-
 namespace CorporateHotelBooking.Test.E2E
 {
     public class HotelTest
     {
-        private readonly HttpClient _httpClient;
-
-        public HotelTest()
-        {
-
-            var testServer = new WebApplicationFactory<Program>();
-            _httpClient = testServer.CreateClient();
-        }
-
         [Fact]
         public async Task should_be_able_to_create_hotel()
         {
-            // Act
-            var response = await _httpClient.PostAsJsonAsync("hotels", new
-            {
-                hotelId = "1",
-                hotelName = "Westing"
-            });
+            // Arrange
+            var hotelService = new HotelService();
+            int hotelId = 1;
+            string hotelName = "Wesing";
 
-            Assert.NotNull(response);
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            // Act
+            var exception = Record.Exception(() => hotelService.AddHotel(hotelId, hotelName));
+
+            // Assert
+            Assert.Null(exception);
+
+        }
+    }
+
+    public class HotelService
+    {
+        public void AddHotel(int hotelId, string hotelName)
+        {
+            throw new NotImplementedException();
         }
     }
 }
