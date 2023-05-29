f3bbf8220009ab7597122a361535d8b9224c7008
Félix Díez <felixeu31@gmail.com>
2023-05-29 10:10:16 +0200

    chore: throw not implemented exception

---
+++ CorporateHotelBooking/CorporateHotelBooking.Api/Controllers/BookingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CorporateHotelBooking.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Book(BookBody bookingData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record BookBody(int RoomNumber, Guid HotelId, Guid EmployeeId, string RoomType, DateTime CheckIn, DateTime CheckOut) { }
+}
---
+++ CorporateHotelBooking/CorporateHotelBooking.Api/Controllers/HotelsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorporateHotelBooking.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddHotel(AddHotelBody hotelData)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{hotelId}/rooms")]
+        public IActionResult SetRoom(Guid hotelId, SetRoomBody roomData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public record AddHotelBody(Guid HotelId, string HotelName) { }
+    public record SetRoomBody(int RoomNumber, string RoomType) { }
+}
